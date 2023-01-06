4a1eb6dd6ed5ce1c96ce471cf1326d1a011ffe50
AlexeyOkhin <k-i-ll-e-r@list.ru>
2023-01-06 18:08:51 +0500

    Initial Commit

---
+++ VK AllCups 2022 part2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VK AllCups 2022 part2
+//
+//  Created by Djinsolobzik on 06.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
